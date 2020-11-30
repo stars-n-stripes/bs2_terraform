da1cc5e4e4421823bfd1d20a862b1189871841ec
James Lynch <jctlynch@gmail.com>
2020-11-30 00:20:33 -0500

    Sketched out DO/scenario/user models

--- downrange/models.py
+++ downrange/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# DO TF provider instance:
+# https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/record
+
+class Droplet(models.Model):
+    # Note: we'll define ALL droplet templates here, regardless of role
+    # Roles will include: Tgt VM, Tgt Docker, Student Gateway, IDS, Student VM
+    # https://www.digitalocean.com/docs/droplets/
+    name = models.CharField(max_length=64, default='do', null=True, blank=True)
+class CloudFirewall(models.Model):
+    # CloudFirewalls will be good for when we need to purely block traffic (i.e., trying to teach tunneling)
+    pass
+class LoadBalancer(models.Model):
+    # May also be used as a firewall that logs in a pinch - will need to figure out how to do that
+    # Ref: https://www.digitalocean.com/docs/networking/load-balancers/#proxy-protocol
+    # Q/A: https://www.digitalocean.com/community/questions/what-logging-is-available-from-cloud-firewalls-load-balancers-and-dns
+    pass
+class DNS:
+    pass
+class VPC:
+    # VPCs will allow us to isolate droplets on a network
+    pass
--- scenarios/models.py
+++ scenarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Scenario(models.Model):
+    """
+    Scenarios are pre-defined terraform states that are composed of one or more DO resources
+    """
+    time_limit = models.TimeField()
+
+class Active(models.Model):
+    # Information about the particular scenario that is currently active for this user (downrange)
+    scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
+    user = models.OneToOneField(User)
+
